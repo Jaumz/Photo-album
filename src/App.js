79cc56b7f67db30b5460654ccc483dfababfe54c
Jo_ao <joaopedro.viana07@gmail.com>
2021-11-26 03:10:18 -0300

    Page commit trial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="https">
+            Photo album
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="https">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="https"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="https">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="https">
+                    Another action
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="https">
+                    Something else here
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="https">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col col-sm col-md -col-lg text-center">
+            <input type="text" class="col-lg-6 col-md-6"></input>
+            <button class="sbutton mt-2 ml-lg-2">Busca</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
